1ce1ab3c20db1984e2eaa0aa5d39ae5fd60fc672
okami-ap <y.okami@alphapolis.co.jp>
2023-09-13 14:52:47 +0900

    名前表示処理修正

--- SecondKadaiApp/ResultViewController.swift
+++ SecondKadaiApp/ResultViewController.swift
@@ -16,7 +16,7 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        name.text = "名前は\(result)です"
+        name.text = "こんにちは、\(result)さん"
     }
     
 
